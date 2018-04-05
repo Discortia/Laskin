b1d0937c6c7dae3a64cb6596e6e71e4531d10c72
AD\1601712 <discortia@gmail.com>
2018-04-05 14:55:36 +0300

    initial commit

---
+++ laskinController.js
@@ -0,0 +1,63 @@
+var laskin = angular.module('laskin',[]);
+
+
+laskin.controller('laskinController', ['$scope', function($scope) {
+	
+$scope.screenLower = '';	
+
+$scope.inputKey = function(x) {
+
+$scope.screenLower = $scope.screenLower + x;
+
+}
+
+$scope.calculate = function(x) {
+$scope.screenUpper = '';	
+$scope.screenUpper = $scope.screenLower + x;
+$scope.screenLower = '';
+
+}
+
+$scope.showResult = function() {
+var y = $scope.screenUpper.substring(0, $scope.screenUpper.length-1);
+var x = $scope.screenLower;	
+var z = $scope.screenUpper.charAt($scope.screenUpper.length-1);
+
+$scope.screenUpper = y + z + x;
+if (z == '+') {
+$scope.screenLower = parseFloat(y) + parseFloat(x);
+}
+
+else if (z == '-') {
+$scope.screenLower = parseFloat(y) - parseFloat(x);
+}
+
+else if (z == '*') {
+$scope.screenLower = parseFloat(y) * parseFloat(x);
+}
+
+else if (z == '/') {
+$scope.screenLower = parseFloat(y) / parseFloat(x);
+}
+
+	
+else {
+	$scope.screenLower = 'the operator used was' + z;
+}
+}	
+
+
+//Clear the whole screen
+$scope.clearScreen = function() {
+	$scope.screenUpper = '';
+	$scope.screenLower = '';
+}
+//Clear only the last character
+$scope.clearLast = function() {
+	var view = $scope.screenLower;
+	
+	$scope.screenLower = view.substring(0, view.length -1);
+}
+
+}
+])
